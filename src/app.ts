475520176139ed147c046da63b120660d5d4ca59
Bhavin <bhavin@code.com>
2020-04-18 14:13:26 +0530

    allow to connect to dev port

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 app.set("port", process.env.PORT);
 app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
