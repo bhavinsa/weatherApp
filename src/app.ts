9459ba72533bec7c5037db2d325a66e1c06fd693
Bhavin <bhavin@code.com>
2020-07-18 17:40:03 +0530

    Express status added

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 app.set("port", process.env.PORT);
 app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
@@ -28,10 +28,45 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
+const config = {
+    title: 'Express Status',
+    path: '/status',
+    spans: [{
+        interval: 1,
+        retention: 60
+    }, {
+        interval: 5,
+        retention: 60
+    }, {
+        interval: 15,
+        retention: 60
+    }],
+    chartVisibility: {
+        cpu: true,
+        mem: true,
+        load: true,
+        responseTime: true,
+        rps: true,
+        statusCodes: true
+    },
+    healthChecks: [
+        {
+            protocol: 'http',
+            host: 'localhost',
+            path: '/test',
+            port: '4000'
+        }
+    ],
+    ignoreStartsWith: '/admin'
+}
 
 /**
  * API routes.
  */
 app.post("/getCityNames", weatherController.getCityNames);
 app.post("/getWeatherData", weatherController.getWeatherData);
+app.use(require('express-status-monitor')(config));
+app.get('/test', (req, res) => {
+    res.send('Hello');
+})
 export default app;
